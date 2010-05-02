b2dc62835c864fccb47bd94404d6e0a0a37bcba3
gantz <gantz@localhost>
2010-05-02 18:35:18 +0200

    First commit of mogre to mercurial repository, based on revision 320 of Mogre svn

---
+++ Mogre/Mogre/Ogre/CLRObject.cpp
@@ -0,0 +1,40 @@
+#include "CLRObject.h"
+#include "CLRConfig.h"
+
+#if LINK_TO_MOGRE
+#pragma comment(lib, "Mogre.lib")
+
+namespace Mogre {
+#define CLROBJECT(T) \
+  __declspec(dllimport) void _Init_CLRObject_##T(CLRObject *pObj);
+#include "CLRObjects.inc"
+#undef CLROBJECT
+}
+
+#define CLROBJECT(T) \
+  void __Init_CLRObject_##T(CLRObject *pObj) { \
+    Mogre::_Init_CLRObject_##T(pObj); \
+  }
+
+namespace Mogre {
+  namespace Implementation {
+    __declspec(dllimport) void FreeCLRObject(void* handle);
+  }
+}
+
+void _FreeCLRObject(void* handle)
+{
+  Mogre::Implementation::FreeCLRObject(handle);
+}
+
+#else
+
+#define CLROBJECT(T) \
+  void __Init_CLRObject_##T(CLRObject *pObj) { }
+
+void _FreeCLRObject(void* handle) { }
+
+#endif
+
+#include "CLRObjects.inc"
+#undef CLROBJECT
